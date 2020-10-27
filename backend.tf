6fe4d18f54dc86a513465c3b42afe13651be31a3
Joseph Beeson <joseph.beeson@dekra.com>
2020-10-27 16:09:51 +0100

    bump backend version

--- backend.tf
+++ backend.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">= 0.12.0"
   required_providers {
-    azurerm = "~> 2.0.0"
+    azurerm = "~> 2.33.0"
   }
   backend "local" {}
 }
 
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "~> 2.0.0"
+  version = "~> 2.33.0"
   features {}
 }
